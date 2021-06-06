0faa44b86b08018df2153f9d668dc8451ed5c6ca
Felipe Buso <felipe_buso@hotmail.com>
2021-06-06 12:16:45 -0300

    exercico 2 incompleto

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ class App extends React.Component {
     super()
     this.state = {
       imageDog: '',
+      lastImageDog: '',
       loading: true,
+      raca: '',
     }
 
     this.fetchDog = this.fetchDog.bind(this);
+    this.extraiRaca = this.extraiRaca.bind(this);
+    // this.loadedDog = this.loadedDog.bind(this);
   }
 
   componentDidMount() {
     this.fetchDog();
   }
+
 
   fetchDog = () => {
     this.setState({ loading: true, },
@@ -23,15 +28,26 @@ class App extends React.Component {
         fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
         .then(data =>{
-          this.setState({
-            imageDog: data.message,
-            loading: false,
-          })
+          const racaAtual = this.extraiRaca(data.message)
+          if (data.message.includes('terrier') === false) {
+            this.setState(({imageDog}) => ({
+              lastImageDog: imageDog,
+              imageDog: data.message,
+              loading: false,
+              raca: racaAtual,
+            }))
+          } 
         } )
       },
-      )
-    
+    )
   }
+
+  extraiRaca(value) {
+    const splitUrl = value.split('/');
+    const raca = splitUrl[4];
+    return raca;
+  }
+
   render() {
     const { imageDog, loading } = this.state;
     const loadingMessage = <span>Loading...</span>
@@ -39,7 +55,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Doguinhos</h1>
         <div className="image-dog">
-          { loading ? loadingMessage : <img src={ imageDog } alt="dog" /> }
+          { loading ? loadingMessage : <img src={imageDog} alt='dog' widt="200px" />}
         </div>
         <div className="button">
           <button type="button" onClick={() => { this.fetchDog() }}>Novo doguinho</button>
